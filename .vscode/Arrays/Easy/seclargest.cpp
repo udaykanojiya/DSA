78dc42c782f39167dfd6e165c2880249b60c8741
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-09-04 08:53:50 +0530

    l

--- .vscode/Arrays/Easy/seclargest.cpp
+++ .vscode/Arrays/Easy/seclargest.cpp
@@ -6,16 +6,16 @@ int main(){
     int max, secmax;
     int arr[10] = {84, 84, 13, 84, 52, 16, 72, 18, 59,84};
     max = arr[0];
-    secmax = INT_MIN;
-    for(int i = 1; i < 10; i++){
-        if(arr[i] > max){
-            secmax = max;
-            max = arr[i];
+    // secmax = INT_MIN;
+    // for(int i = 1; i < 10; i++){
+    //     if(arr[i] > max){
+    //         secmax = max;
+    //         max = arr[i];
+    //     }
+    //     else if(arr[i] > secmax && arr[i] != max){
+    //         secmax = arr[i];
         }
-        else if(arr[i] > secmax && arr[i] != max){
-            secmax = arr[i];
-        }
-    // }
+    }
     cout << "The largest element in the array is: " << max << endl;
     cout << "The second largest element in the array is: " << secmax << endl;
     return 0;
